54eafff15aaff1d9cf841c1a944716fd890e657b
ThomasTorres2024 <thomastorres2024@gmail.com>
2025-08-31 05:50:14 -0400

    Worked more on the UI, going to work on getting communication between user and a deepseek LLM going tomorrow, then I have to do some work with rendering markdown

--- app.js
+++ app.js
@@ -13,5 +13,29 @@ app.get('',(rec,res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+app.set('views','./views');
+app.set('view engine','ejs');
+app.use(express.json());
+
+/**
+ * Post request for when the user queries deepseek 
+ */
+app.post('/reqDeepseek',(req,res) =>{
+
+  const userQuery = req.body['userQuery'];
+  res.send(userQuery);
+});
+
+
+
+//llama sample code for tomorrow 
+// import ollama from 'ollama'
+
+// const response = await ollama.chat({
+//   model: 'llama2',
+//   messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+// })
+// console.log(response.message.content)
+
 //launch locally 
 app.listen(port, () => console.info(`Listening on port: ${port}`));
---
+++ public/js/index.js
@@ -0,0 +1,114 @@
+/**
+ * Changes the screen from having a logo with a chatbox in the center to a screen where
+ * the chatbox is at the bottom and there is a conversation going on between the user
+ * and Deepseek/LLM
+ */
+function setScreenTextResponses(){
+
+}
+
+function resetTextAreaForm(){
+    document.getElementById("chatBox").style.fontFamily="system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+    document.getElementById("chatBox").style.color="gray";
+    document.getElementById("chatBox").style.fontSize="20";    
+}
+
+function convertSubmitToChange(){
+    const submitButton = document.getElementById("voiceButton");
+    submitButton.classList.replace('submitButton','loadingButton');
+    image = voiceButton.querySelector('img'); // First <img> inside that button
+    image.src="img/loading_center.png"
+}
+
+function convertVoiceTosubmit(){
+    const voiceButton = document.getElementById("voiceButton");
+    voiceButton.classList.replace('voiceButtonClass', 'submitButton');
+    voiceButton.childNodes[2].textContent="";
+    image = voiceButton.querySelector('img'); // First <img> inside that button
+    image.src="img/arrow_submit.png"
+}
+
+function convertSubmitToVoice(){
+    const submitButton = document.getElementById("voiceButton");
+    submitButton.classList.replace('loadingButton','voiceButtonClass');
+    submitButton.classList.replace('submitButton','voiceButtonClass');
+    voiceButton.childNodes[2].textContent="Voice";
+    image = voiceButton.querySelector('img'); // First <img> inside that button
+    image.src="img/volume_logo.png"
+}
+
+function main(){
+
+    var voiceButtonModified = false; 
+
+    //text book event listener, changes "voice" button to modify button 
+    const textBoxEvent = document.getElementById("chatBox");
+    textBoxEvent.addEventListener("input",()=>{
+        //check if size of input is back to 0 
+        if(voiceButtonModified)
+        {
+            //revert submit button back to voice button
+            if(document.getElementById("chatBox").value.length==0){
+                convertSubmitToVoice();
+                resetTextAreaForm();
+                voiceButtonModified = false; 
+            }
+        }
+        //convert voice button to submit button
+        else
+        {
+            //change size of font, color, and font family of form as well
+            document.getElementById("chatBox").style.fontFamily="sans-serif";
+            document.getElementById("chatBox").style.color="black";
+            document.getElementById("chatBox").style.fontSize="12";
+
+            convertVoiceTosubmit();
+            voiceButtonModified=true;
+        }
+    });
+
+    
+    const form = document.getElementById('formChatBox');
+
+    form.addEventListener('submit', function(event) {
+        // Prevent buttons from submitting in form when pressed
+        event.preventDefault();
+
+        //change submit button to waiting button
+
+        //send 
+    });
+
+    const voiceButton = document.getElementById("voiceButton");
+    voiceButton.addEventListener("click", async ()=>{
+        if(voiceButtonModified){
+            const userQuery = document.getElementById("chatBox").value;
+
+
+            convertSubmitToChange();
+            document.getElementById("chatBox").value = "";
+            resetTextAreaForm();
+            //send request to server for info 
+            
+            const options = {
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body : JSON.stringify({userQuery})};
+            
+            const res = await fetch("/reqDeepseek",options);
+            const result = await res.text();
+            
+            console.log(result);
+
+            voiceButtonModified = false; 
+            //once server request is done, we can go back to voice
+            convertSubmitToVoice();
+
+        }
+    });
+
+    //add listener for entering 
+
+}
+
+main();
